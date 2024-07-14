7fa977c129e2af10fef3535cff7d403c2c6f5942
Phillip Lam <plam89@ucla.edu>
2024-07-13 21:22:52 -0700

    free form drawing 90% done

--- index.js
+++ index.js
@@ -8,18 +8,73 @@ myCanvas.width = canvasProperties.width
 myCanvas.height = canvasProperties.height
 
 const ctx = myCanvas.getContext('2d')
-ctx.fillStyle = 'gray'
-ctx.fillRect(0,0, myCanvas.width, myCanvas.height) //grey outside area
+clearAndRedrawCanvas()
 
-const stageProperties = {
-    width: 600,
-    height: 480,
-    left: canvasProperties.center.x - 600 / 2,
-    right: canvasProperties.center.y - 480 / 2
+const shapes = []
+let path = []
+
+
+myCanvas.onpointerdown = function(e) {
+   
+   const mousePos = {
+    x: e.offsetX,
+    y: e.offsetY
+   }
+   path.push(mousePos)
+
+const moveCallBack = function(e) {
+    const mousePos = {
+     x: e.offsetX,
+     y: e.offsetY
+    }
+   path.push(mousePos)
+ }
+
+ 
+ const upCallBack = function(e) {
+    myCanvas.onpointermove = null //notice using .on will not let you use removeEventListener but you can set its .on property to null
+    myCanvas.onpointerup = null
+ 
+    // myCanvas.removeEventListener('pointermove', moveCallBack) //must remove these listeners so no spam lines are drawn 
+    // myCanvas.removeEventListener('pointerup', upCallBack)
+
+    shapes.push(path)
+    path = []
+
+    clearAndRedrawCanvas()
+    for (const shape of shapes) {
+        ctx.beginPath()
+        ctx.moveTo(shape[0].x, shape[0].y)
+        
+        for(let i = 1; i < shape.length;i++) { //start at index 1 since we have initial positions of index 0 already
+            ctx.lineTo(shape[i].x, shape[i].y)
+        }       
+        ctx.stroke()
+    }
+
+ }
+//  myCanvas.addEventListener('pointermove', moveCallBack)
+//  myCanvas.addEventListener('pointerup', upCallBack)
+ myCanvas.onpointermove = moveCallBack
+ myCanvas.onpointerup = upCallBack
+ 
 }
 
-ctx.fillStyle = 'white'
-ctx.fillRect(stageProperties.left,  //our center drawing canvas
-            stageProperties.right, 
-            stageProperties.width, 
-            stageProperties.height)
+function clearAndRedrawCanvas() {    
+    ctx.clearRect(0,0,myCanvas.width, myCanvas.height)
+    ctx.fillStyle = 'gray'
+    ctx.fillRect(0,0, myCanvas.width, myCanvas.height) //grey outside area
+
+    const stageProperties = {
+        width: 600,
+        height: 480,
+        left: canvasProperties.center.x - 600 / 2,
+        right: canvasProperties.center.y - 480 / 2
+    }
+
+    ctx.fillStyle = 'white'
+    ctx.fillRect(stageProperties.left,  //our center drawing canvas
+                stageProperties.right, 
+                stageProperties.width, 
+                stageProperties.height)
+}
