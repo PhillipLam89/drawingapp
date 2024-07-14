0233b022430926bf378c326b4696c3a7131c158d
Phillip Lam <plam89@ucla.edu>
2024-07-14 13:05:55 -0700

    added squares option

--- shapes/rect.js
+++ shapes/rect.js
@@ -1,7 +1,8 @@
 class Rect {
-    constructor(corner1) {
+    constructor(corner1, type) {
         this.corner1 =  corner1
         this.corner2 = corner1
+        this.type = type
     }
 
     setCorner2(corner2) {
@@ -13,7 +14,7 @@ class Rect {
         const minY = Math.min(this.corner1.y, this.corner2.y)
         const width = Math.abs(this.corner1.x - this.corner2.x)
         const height = Math.abs(this.corner1.y - this.corner2.y)
-        ctx.rect(minX,minY,width,height)
+        ctx.rect(minX,minY,width, (this.type === 'square' ? width : height) )
         ctx.stroke()
     }
 }
