da716f8832a830381d06bce79741762234942f80
Phillip Lam <plam89@ucla.edu>
2024-07-17 23:08:16 -0700

    curr

--- shapes/path.js
+++ shapes/path.js
@@ -36,12 +36,10 @@ class Path extends Shape {
 
         ctx.save()
         ctx.strokeStyle = 'red'
-        ctx.lineWidth = this.options.strokeWidth
-      
+        ctx.lineWidth = this.options.strokeWidth   
         ctx.beginPath()
      
         ctx.moveTo(this.points[0].x, this.points[0].y)
-
         for(let i = 1; i < this.points.length;i++) { //start at index 1 since we have initial positions of index 0 already
             ctx.lineTo(this.points[i].x, this.points[i].y)
 
